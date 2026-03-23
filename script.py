96ba106ba3f777cc7d1705166ad82c482036e0a6
Oussama Filani <42185573+filani07@users.noreply.github.com>
2026-03-23 18:44:18 +0100

    fix: specify both   columns=/index= and axis= simultaneously

--- script.py
+++ script.py
@@ -47,14 +47,14 @@ for col in ['Age', 'CreditScore', 'Balance', 'EstimatedSalary']:
 
 
 ## Drop first 3 features
-df.drop(columns=['RowNumber', 'CustomerId', 'Surname'], axis=1, inplace=True)
+df.drop(columns=['RowNumber', 'CustomerId', 'Surname'], inplace=True)
 
 ## Filtering using Age Feature using threshold
-df.drop(index=df[df['Age'] > 80].index.tolist(), axis=0, inplace=True)
+df.drop(index=df[df['Age'] > 80].index.tolist(), inplace=True)
 
 
 ## To features and target
-X = df.drop(columns=['Exited'], axis=1)
+X = df.drop(columns=['Exited'])
 y = df['Exited']
 
 ## Split to train and test
